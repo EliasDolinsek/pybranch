766d21304a82f7c8cd23afc25824b50bc50a4044
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-28 13:43:04 +0100

    Handle no repository

--- pybranch.py
+++ pybranch.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 
+from git.exc import InvalidGitRepositoryError
 from git import Repo
 from simple_term_menu import TerminalMenu
 
@@ -8,16 +9,25 @@ new_branch_shortcut = "ctrl-n"
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        repo = Repo(sys.argv[1])
+        path = sys.argv[1]
+        try:
+            repo = Repo(path)
+        except InvalidGitRepositoryError:
+            print(f"No git repository found in {path}")
+            exit()
     else:
-        repo = Repo()
+        try:
+            repo = Repo()
+        except InvalidGitRepositoryError:
+            print("Not git repository found in this directory")
+            exit()
 
     all_branches = [head.name for head in repo.heads]
     all_branches.remove(repo.active_branch.name)
     all_branches = [repo.active_branch.name + " (active)"] + all_branches
 
     terminal_menu = TerminalMenu(all_branches, title=f"Switch branch ({new_branch_shortcut} for creating a new branch)",
-                                 accept_keys=("ctrl-n", "enter"))
+                                 accept_keys=(new_branch_shortcut, "enter"))
 
     index = terminal_menu.show()
     accept_key = terminal_menu.chosen_accept_key
@@ -27,6 +37,7 @@ if __name__ == "__main__":
             print("Remaining on same branch")
         else:
             subprocess.run(["git", "checkout", all_branches[index]])
-    else:
+
+    if accept_key == new_branch_shortcut:
         name = input("Branch name: ")
         subprocess.run(["git", "checkout", "-b", name])
